d65383e8ded03fb64fa66f2634ecceae04fb115a
perrybates <perryboateng08@gmail.com>
2022-01-21 09:48:35 -0800

    added email backend

--- leads/views.py
+++ leads/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, reverse
+from django.core.mail import send_mail
 from django.http import HttpResponse
 from django.views import generic 
 from .models import Lead, Agent
@@ -24,10 +25,20 @@ class LeadCreateView(generic.CreateView):
     form_class =  LeadModelForm
 
     def get_success_url(self):
-        return reverse("leads:lead-list")
-
+        return reverse("leads:lead-list")  
+    
+    def form_valid(self, form):
+        send_mail(
+            subject="A lead has been created",
+            message="Go to the site to see the new lead",
+            from_email="test@test.com",
+            recipient_list =["perrybates@bates.com"]
+        )
+        return super(LeadCreateView, self).form_valid(form)   
+ 
 
 class LeadUpdateView(generic.UpdateView):
+
     template_name = "leads/lead_update.html"
     queryset =  Lead.objects.all() 
     form_class = LeadModelForm
